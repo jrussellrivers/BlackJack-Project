efe0f486c0b680cdeddfe124f5c0c8a51d08c401
Jordan Rivers <j.russell.rivers@gmail.com>
2020-06-25 15:10:21 -0400

    added shuffle feature

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 // Building a deck
+
 let suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
 let cardNums = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-let deck = []
 
 function createDeck(){
     let deck = []
@@ -22,3 +22,31 @@ function createDeck(){
 }
 
 console.log(createDeck())
+
+// Shuffle formula
+// I used this formula because it was highly reccommended for shuffling an array:
+// https://github.com/Daplie/knuth-shuffle/blob/master/index.js
+
+function shuffle(array) {
+    let currentIndex = array.length
+    let temporaryValue
+    let randomIndex
+    
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+let shuffledDeck = shuffle(createDeck())
+console.log(shuffledDeck)
